5ac649183d96fa0270036c6e69b31513b19a3825
mzxl1987 <448632824@qq.com>
2019-07-26 09:05:16 +0800

    eureka server 搭建

---
+++ springBoot_parent/springBoot_eurekaserver/src/main/java/com/mego/eurekaserver/ServletConfig.java
@@ -0,0 +1,21 @@
+package com.mego.eurekaserver;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServletConfig   {
+	
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                factory.setPort(8761);
+            }
+        };
+    }
+
+}
